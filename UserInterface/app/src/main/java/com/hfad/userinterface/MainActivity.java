d80e524b23dedef3d9f87f0b90e2984cd93102c1
Daya Adianto <daya.adianto@uqconnect.edu.au>
2016-08-09 23:14:31 +0700

    Add scroll view and try creating toasts

--- UserInterface/app/src/main/java/com/hfad/userinterface/MainActivity.java
+++ UserInterface/app/src/main/java/com/hfad/userinterface/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfad.userinterface;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Using IDE feature to create Toast
+        Toast.makeText(MainActivity.this, "This is a toast. Using Android Studio",
+                Toast.LENGTH_SHORT).show();
+
+        // Manually create Toast
+        CharSequence text = "This is a toast. Manually created.";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
     }
 }
