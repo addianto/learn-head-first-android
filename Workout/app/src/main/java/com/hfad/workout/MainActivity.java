63e12b9484561e638960c473e6dea698012f376f
Daya Adianto <daya.adianto@uqconnect.edu.au>
2016-08-13 00:32:07 +0700

    Create workout detail fragment

--- Workout/app/src/main/java/com/hfad/workout/MainActivity.java
+++ Workout/app/src/main/java/com/hfad/workout/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Load the fragment to be included into activity
+        // Had to use supportFragmentManager instead of the usual fragmentManager
+        // Consequently, the Activity class had to extends AppCompatActivity instead of Activity
+        WorkoutDetailFragment fragment = (WorkoutDetailFragment)
+                getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        fragment.setWorkoutId(1);
     }
 }
