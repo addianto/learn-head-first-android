6cf84e487003aee820fd318476d3aac43b041f1a
Daya Adianto <daya.adianto@uqconnect.edu.au>
2016-09-21 11:36:28 +0700

    Add Create Order menu item and its drawable icon

--- BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -2,6 +2,8 @@ package com.hfad.bitsandpizzas;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,28 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is
+        // present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                // Code to run when the Create Order item is clicked
+                return true;
+            case R.id.action_settings:
+                // Code to run when the settings item is clicked
+                // Returning true tells Android you've dealt with the item being clicked
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
