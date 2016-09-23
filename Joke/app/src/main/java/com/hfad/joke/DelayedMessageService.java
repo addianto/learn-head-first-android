e10f082d09b9f42696b7cb6f5967191f11e66762
Daya Adianto <daya.adianto@uqconnect.edu.au>
2016-09-23 22:58:02 +0700

    Add log call

--- Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
+++ Joke/app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -2,8 +2,12 @@ package com.hfad.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class DelayedMessageService extends IntentService {
+
+    private static final long TEN_SECONDS = 10000;
+    public static final String EXTRA_MESSAGE = "message";
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -11,6 +15,19 @@ public class DelayedMessageService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        // Code to do something
+        synchronized (this) {
+            try {
+                wait(TEN_SECONDS);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
+    }
+
+    private void showText(final String text) {
+        Log.v("DelayedMessageService", "The message is " + text);
     }
 }
